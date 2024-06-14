4ea49a141db6056564d2b25e2cc84992ae97d89f
T-Prakash2002 <prakasht1405@gmail.com>
2024-06-14 20:37:21 +0530

    sample data card

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-
-const fruits=['orange','banana','kivi','mango']
-
-
-fruits.splice(1,1,"apple") // update particular array index
-
-
-console.log(fruits)
-
+// 
+// const fruits=['orange','banana','kivi','mango']
+// 
+// 
+// fruits.splice(1,1,"apple") // update particular array index
+// 
+// 
+// console.log(fruits)
+// 
