943a41c58ceb9e27035024ed7653c0646e7f073c
T-Prakash2002 <prakasht1405@gmail.com>
2024-06-14 20:24:56 +0530

    array splice method

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-// localStorage.setItem('settings', { "theme": 'dark', "layout": 'grid' });
-// console.log(localStorage.getItem('settings'));
-// 
+
+const fruits=['orange','banana','kivi','mango']
+
+
+fruits.splice(1,1,"apple") // update particular array index
+
+
+console.log(fruits)
+
