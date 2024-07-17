c7039be1ba676d426d1a8dd867503f63cf3eb926
T-Prakash2002 <prakasht1405@gmail.com>
2024-07-17 11:05:43 +0530

    call,bind,apply extra example

--- script.js
+++ script.js
@@ -1,24 +1,77 @@
+// 
+// function sayName(batch){
+//     return `Hey ${this.name} are you starting ${batch}?`
+// }
+// 
+// var obj={
+//     name:"CALL"
+// }
+// var obj2={
+//     name:"APPLY"
+// }
+// var obj3={
+//     name:"BIND"
+// }
+// // CALL 
+// console.log(sayName.call(obj,'cf'))
+// 
+// //APPLY 
+// console.log(sayName.apply(obj2,['cf2']))
+// 
+// //BIND
+// const sayFunc=sayName.bind(obj3,"cf3")
+// 
+// console.log(sayFunc())
 
-function sayName(batch){
-    return `Hey ${this.name} are you starting ${batch}?`
+// const market=[
+//     {
+//         shop_name:"Shop1",
+//         special:"Mango"
+//     },{
+//         shop_name:"Shop2",
+//         special:"Apple"
+//     }
+// ]
+// 
+// function printMarkets(index){
+//     this.show=function(){
+//         console.log(`${this.shop_name} : ${this.special}`)
+//     }
+//     this.show()
+// }
+
+// for(let i=0;i<market.length;i++){
+//     printMarkets.call(
+//         market[i]
+//     )
+// }
+
+const arr=[1,2,3]
+const ele=[4,5,6]
+
+arr.push.apply(arr,ele)
+
+console.log(arr)
+
+function add(num){
+    num+5
+}
+function sub(num){
+    num-3
+}
+function mul(num){
+    num*3
 }
 
-var obj={
-    name:"CALL"
-}
-var obj2={
-    name:"APPLY"
-}
-var obj3={
-    name:"BIND"
-}
-// CALL 
-console.log(sayName.call(obj,'cf'))
+const iterator=calculate(add,sub,mul)
+console.log(iterator(7));
 
-//APPLY 
-console.log(sayName.apply(obj2,['cf2']))
-
-//BIND
-const sayFunc=sayName.bind(obj3,"cf3")
-
-console.log(sayFunc())
+function calculate(...funcs){
+    return function (firstNum){
+        let res=firstNum
+        for(let i=funcs.length-1;i>=0;i--){
+            res=funcs[i](res)
+        }
+        return res
+    }
+}
